3fdbe983ec14b74e976ccb8caaf27ad96abd1a6e
PattiDonahue <pattidonahue1@gmail.com>
2015-08-14 15:17:12 -0400

    Initial Commit

---
+++ Shapes/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Shapes
+//
+//  Created by Patti Donahue on 8/14/15.
+//  Copyright (c) 2015 Patti Donahue. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
